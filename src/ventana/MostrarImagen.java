e1ae533a67c983c5175288ba8231856de6b3ad28
jach17 <jonathan.aldana170500@gmail.com>
2020-10-23 19:41:30 -0500

    Imagen dentro del panel, falta rotar

--- src/ventana/MostrarImagen.java
+++ src/ventana/MostrarImagen.java
@@ -4,13 +4,18 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.awt.image.CropImageFilter;
 import java.awt.image.FilteredImageSource;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class MostrarImagen extends JFrame {
@@ -19,6 +24,9 @@ public class MostrarImagen extends JFrame {
     int width, height;
     Image imgRecorted;
     JPanel p;
+    JLabel lbl;
+    ImageIcon im;
+    JButton btnRotate;
 
     public MostrarImagen() {
         this.pack();
@@ -27,30 +35,60 @@ public class MostrarImagen extends JFrame {
         this.setLayout(null);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        cargarImagen();
+        this.setBackground(Color.yellow);
+        
         p = new JPanel();
-        p.setBounds(300,300, 300,300);
+        lbl = new JLabel();
+        btnRotate = new JButton("Click to rotate");
+        
+        
+        cargarImagen();
+        
+
+        btnRotate.setBounds(20, 400, 150, 40);
+        btnRotate.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                clickeado();
+            }
+        });
+        
+        lbl.setIcon(im);
+        lbl.setBounds(0, 0, 200, 200);
+        lbl.setVisible(true);
+
+        p.setBounds(200, 100, 200, 200);
         p.setBackground(Color.yellow);
+        p.setLayout(null);
+        
+        
+        p.add(lbl);
+        this.add(btnRotate);
         this.add(p);
     }
-
-    @Override
-    public void paint(Graphics g) {
-        super.paint(g); //To change body of generated methods, choose Tools | Templates.
-        g.drawImage(imgRecorted, 0, 0, null);
-
+    
+    public void clickeado(){
+        System.out.println("You clicked me!");
     }
+//
+//    @Override
+//    public void paint(Graphics g) {
+//        super.paint(g); //To change body of generated methods, choose Tools | Templates.
+//        g.drawImage(imgRecorted, 10, 10, null);
+//
+//    }
 
     public void cargarImagen() {
         try {
             imagenPrincipal = ImageIO.read(new File("src/img/Compromiso.png"));
             width = imagenPrincipal.getWidth();
             height = imagenPrincipal.getHeight();
-            imgRecorted = createImage(new FilteredImageSource(imagenPrincipal.getSource(), new CropImageFilter(250, 0, 300,300)));
+            imgRecorted = createImage(new FilteredImageSource(imagenPrincipal.getSource(), new CropImageFilter(250, 0, 200, 200)));
+            im = new ImageIcon(imgRecorted);
+
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }
 
 }
-
