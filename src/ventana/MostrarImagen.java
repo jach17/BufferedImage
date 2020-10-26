738143074577cfa7f4f506b01df1e12086f78af7
Jonathan <jonathan@stardust.com.mx>
2020-10-26 00:43:20 -0600

    La imagen ya gira

--- src/ventana/MostrarImagen.java
+++ src/ventana/MostrarImagen.java
@@ -1,17 +1,10 @@
 package ventana;
 
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.GraphicsConfiguration;
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
 import java.awt.Image;
-import java.awt.Transparency;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.awt.image.CropImageFilter;
 import java.awt.image.FilteredImageSource;
@@ -23,14 +16,12 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import sun.awt.image.FileImageSource;
 
 public class MostrarImagen extends JFrame {
 
-    
     BufferedImage imagenPrincipal, imagenPrincipalRotada, imagenPrincipalRecortada;
     int width, height;
-    Image imgRecorted, imgRotated;
+    Image imgRecorted = null, imgRotated;
     JPanel p;
     JLabel lbl;
     ImageIcon im;
@@ -53,8 +44,7 @@ public class MostrarImagen extends JFrame {
             clickeado();
         });
 
-        p.setBounds(200, 100, 200, 200);
-        p.setBackground(Color.yellow);
+        p.setBounds(100,100, 200,200);
         p.setLayout(null);
 
         lbl.setBounds(0, 0, 200, 200);
@@ -63,10 +53,12 @@ public class MostrarImagen extends JFrame {
         p.add(lbl);
         this.add(btnRotate);
         this.add(p);
+        
     }
 
     public void clickeado() {
         cargarImagen();
+        pintar(p.getGraphics());
     }
 
     public Image recortarImagen(BufferedImage imagenPrincipal) {
@@ -74,13 +66,53 @@ public class MostrarImagen extends JFrame {
         return imgRecorted; //<-Esta es la imagen que enviamos al lbl
     }
 
+    int grados = 0;
+    
+    public void pintar(Graphics g) {
+
+        if (imgRecorted != null) {
+            if(grados<360){
+                grados +=90;
+            }else{
+                grados=90;
+            }
+            BufferedImage bufimRecorted = imgRecortedToBuffer(imgRecorted);
+            Graphics2D g2d = (Graphics2D) g;
+            
+            g2d.translate(p.getSize().width/2, p.getSize().height/2);
+            g2d.rotate(Math.toRadians(grados));
+            g2d.drawImage(bufimRecorted, -bufimRecorted.getWidth() / 2, -bufimRecorted.getHeight() / 2, null);
+
+        } else {
+            System.out.println("imgRecorted not init");
+        }
+    }
+
+    public BufferedImage imgRecortedToBuffer(Image img) {
+        if (img instanceof BufferedImage) {
+            return (BufferedImage) img;
+        }
+
+        // Create a buffered image with transparency
+        BufferedImage bimage = new BufferedImage(200, 200, imagenPrincipal.getType());
+
+        // Draw the image on to the buffered image
+        Graphics2D bGr = bimage.createGraphics();
+        bGr.drawImage(img, 0, 0, null);
+        bGr.dispose();
+
+        // Return the buffered image
+        return bimage;
+    }
+
     public void cargarImagen() {
         try {
             imagenPrincipal = ImageIO.read(new File("src/img/Compromiso.png"));
             imgRecorted = recortarImagen(imagenPrincipal);
-
-            im = new ImageIcon(imgRecorted);
-            lbl.setIcon(im);
+            
+//
+//            im = new ImageIcon(imgRecorted);
+//            lbl.setIcon(im);
 
         } catch (IOException e) {
             System.out.println(e.getMessage());
